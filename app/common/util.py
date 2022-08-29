9ae8a3e8e228a3d3663438af6f72f671eb3946d1
namvh <namvh@smartosc.com>
2022-08-29 10:26:45 +0700

    validate id does not exist

--- app/common/util.py
+++ app/common/util.py
@@ -8,3 +8,12 @@ def validate_unique(table, field, **kwargs):
     if q:
         raise ValueError(f"""'{q}' already exist""")
     return kwargs[field]
+
+
+def validate_id(table, field, **kwargs):
+    session = SessionLocal()
+    q = session.query(getattr(table, field)).filter_by(**kwargs).scalar()
+    session.close()
+    if not q:
+        raise ValueError(f"""'{kwargs[field]}' does not exist""")
+    return kwargs[field]
--- app/schemas/company_model.py
+++ app/schemas/company_model.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, confloat, conint, constr, validator
 
-from app.common.util import validate_unique
+from app.common.util import validate_id, validate_unique
 from app.models.company_model import CompanyModel
 from app.schemas.base import BaseModelSchemas
 
@@ -35,6 +35,10 @@ class CompanyModelUpdate(CompanyModelBase):
     @validator("name")
     def unique_check_model(cls, name_check):
         return validate_unique(CompanyModel, "name", name=name_check)
+
+    @validator("id")
+    def unique_check_id(cls, id):
+        return validate_id(CompanyModel, "id", id=id)
 
 
 class CompanyModelResponse(CompanyModelBase):
